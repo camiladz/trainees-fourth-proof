1304c261b4e5b76258ad760ccf4cf6de00772714
camiladz <camifstack@gmail.com>
2023-02-27 10:53:39 -0300

    add client - apolloclient

--- teste-b8one/src/main.tsx
+++ teste-b8one/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "https://trainees-fourth-proof-igode.ondigitalocean.app/graphql",
+  cache: new InMemoryCache({
+    resultCaching: false,
+  })
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ApolloProvider>
 )
